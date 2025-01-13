d2b28242383943eb2f2d91372cb39fbf86ae0478
loosemotion69 <jintubaro00@gmail.com>
2025-01-13 19:03:14 +0530

    Add files via upload
    we have modernize  the Rcare4u

---
+++ Rcare4u/index.js
@@ -0,0 +1,6 @@
+document.querySelector('.user').addEventListener('click', ()=>{
+    document.querySelector('.acc').style.right = 0;
+})
+document.querySelector('.xmark').addEventListener('click', ()=>{
+    document.querySelector('.acc').style.right = '-100%';
+})
